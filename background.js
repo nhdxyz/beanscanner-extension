fc07c5d1c3b2d68456c0949aabc03e5c11ee7388
Nathan Dye <nate9402@gmail.com>
2024-05-14 22:19:19 -0400

    Buy works, need to make token add resolve

--- background.js
+++ background.js
@@ -20,7 +20,7 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
     console.log(`Selected text: ${token}`);
     console.log(`Token length: ${token.length}`);
     if (token.length >= 32 && token.length <= 44) {
-      const url = `https://beanscanner.xyz/token/${token}`;
+      const url = `http://localhost:3000/token/${token}`;
       chrome.tabs.create({ url });
     } else {
       chrome.scripting.executeScript({
@@ -34,11 +34,34 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
 });
 
 chrome.commands.onCommand.addListener((command) => {
-  if (command === "analyze") {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      const activeTab = tabs[0];
-      const url = new URL(activeTab.url);
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const activeTab = tabs[0];
+    const url = new URL(activeTab.url);
 
+    if (command === "analyze") {
+      let token = null;
+      if (url.hostname === 'dexscreener.com' && url.pathname.includes('/solana/')) {
+        token = url.pathname.split('/solana/')[1];
+      } else if (url.hostname === 'photon-sol.tinyastro.io' && url.pathname.includes('/en/lp/')) {
+        token = url.pathname.split('/en/lp/')[1];
+        const queryIndex = token.indexOf('?');
+        if (queryIndex !== -1) {
+          token = token.substring(0, queryIndex);
+        }
+      }
+
+      if (token && token.length >= 32 && token.length <= 44) {
+        const beanScannerUrl = `http://localhost:3000/token/${token}`;
+        chrome.tabs.create({ url: beanScannerUrl });
+      } else {
+        chrome.scripting.executeScript({
+          target: { tabId: activeTab.id },
+          function: () => {
+            alert('The extracted token is not valid (must be between 32 and 44 characters long).');
+          }
+        });
+      }
+    } else if (command === "buy") {
       chrome.storage.sync.get(['selectedBot'], (result) => {
         let baseUrl;
         switch (result.selectedBot) {
@@ -78,6 +101,6 @@ chrome.commands.onCommand.addListener((command) => {
           });
         }
       });
-    });
-  }
+    }
+  });
 });
